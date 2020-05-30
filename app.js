5feb933f09368c8b4ce8f36da8bb9f10b5f1bea6
lantanakamara <asadatnish@gmail.com>
2020-05-30 14:56:28 -0400

    final commit for the javascript file

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const {hash} = window.location;
+const message = atob(hash.replace('#', ''));
+
+if(message)
+{
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+  document.querySelector('#message').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', (event) =>
 {
   event.preventDefault();
@@ -12,10 +22,24 @@ document.querySelector('form').addEventListener('submit', (event) =>
 
 })
 
+//copy link function
 const copyContent = () =>
 {
   const text = document.querySelector('#link-input');
   text.select();
   document.execCommand('copy');
+
+  setTimeout(() =>
+  {
+    // document.querySelector('#link-form').classList.add('hide');
+    document.querySelector('#alert').classList.remove('hide');
+  },100);
+  
+  setTimeout(() =>
+  {
+    document.querySelector('#alert').classList.add('hide');
+    //location.reload();
+  }, 1000);
+  
 }
 document.querySelector('#copy').addEventListener('click', copyContent);
