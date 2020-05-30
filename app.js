5ae5ec6b82919920f826cde91bc8d0581d59052e
lantanakamara <asadatnish@gmail.com>
2020-05-30 13:04:36 -0400

    addes the logic of targeting the user input and encrypt and then append it to the URL link that can be shared with other people who will then get access to the secret message.

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ document.querySelector('form').addEventListener('submit', (event) =>
 {
   event.preventDefault();
 
-  const input = document.querySelector('input');
-  
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+  const input = document.querySelector('#message-input');
+  const encoded = btoa(input.value);
+  const linkInput = document.querySelector('#link-input').value
+   = `${window.location}#${encoded}`;
+
+
 })
+
+const copyContent = () =>
+{
+  const text = document.querySelector('#link-input');
+  text.select();
+  document.execCommand('copy');
+}
+document.querySelector('#copy').addEventListener('click', copyContent);
